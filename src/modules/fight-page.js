64f1cf24a9971dc1846b6e1ed2c74456d35631ea
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-09-18 12:05:21 +0100

    refactored fightPage to use reacts state variable hack for refresh
    Co-authored-by: shaya <akshayasatheesh@hotmail.co.uk>

--- src/modules/fight-page.js
+++ src/modules/fight-page.js
@@ -5,23 +5,26 @@ import { getRandomFishData } from "./fetch-helper";
 const FightPage = () => {
   const [fishData, setFishData] = React.useState(null);
   // const [opponentFishData, setOpponentFishData] = React.useState(null);   --> Accept button
+  const [count, setCount] = React.useState(0);
 
   // button activated effect - rejects button
   React.useEffect(() => {
     getRandomFishData().then((data) => {
       setFishData(data);
     });
-  }, []);
+  }, [count]);
   return (
     <>
       <h1 className="fight__title">Fishcious choice, player.</h1>
       <FishCard {...fishData} />
       <button className="accept__btn">I'm hooked - Accept.</button>
-      <button className="reject__btn"
+      <button
+        className="reject__btn"
         onClick={() => {
-          getRandomFishData().then((data) => {
-            setFishData(data);
-          });
+          // Increases state variable (count) by 1 b/c it is a dependency (line 15) for our api data effect
+          // Changing count runs the effect (b/c its a dependency)
+          // Effect gives us new random fish data every time its run
+          setCount(count + 1); //number (+1) doesn't matter as long as its not none
         }}
       >
         A load of pollocks! - Choose again.
